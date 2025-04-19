547c78b48abcd80d291c17fd2a5e1a91d2251187
abhisheknagpure <110444541+abhisheknagpure@users.noreply.github.com>
2025-04-19 13:23:13 +0530

    Update s3_sample_bucket_test.tf

--- sandbox/terraform/s3_sample_bucket_test.tf
+++ sandbox/terraform/s3_sample_bucket_test.tf
@@ -1,5 +1,3 @@
-data "aws_caller_identity" "current" {}
-
 resource "aws_s3_bucket" "sandbox_dev_test_bucket" {
   bucket = "aft-sandbox-dev-test-${data.aws_caller_identity.current.account_id}"
   acl    = "private"
