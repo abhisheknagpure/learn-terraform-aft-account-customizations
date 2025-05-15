6515dfe8abfbb0a49cd563e0aa4360d0a70656e0
abhisheknagpure <110444541+abhisheknagpure@users.noreply.github.com>
2025-05-15 21:00:06 +0530

    remove

--- security/terraform/data.tf
+++
@@ -1,6 +0,0 @@
-data "aws_region" "current" {}
-data "aws_caller_identity" "current" {}
-data "aws_partition" "current" {}
-data "aws_organizations_organization" "roots" {
-count = local.is_organization_onboarding ? 1 : 0
-}
